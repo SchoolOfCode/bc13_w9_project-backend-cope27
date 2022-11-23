63fe6aedd30e7b477692477bb0fcfb8dd703239b
“stephaniezai” <jupitergalilean4@gmail.com>
2022-11-23 15:02:21 +0000

    Create and Read completed

--- sql/posts.sql
+++ sql/posts.sql
@@ -10,11 +10,39 @@ CREATE TABLE posts (
 INSERT INTO posts
 (userName, projectGoal, projectType, projectTools, collaborators)
 VALUES
-('Jimmy08','Lord of the Rings API', 'Build', 'React', 1),
-('Lion_KingFan_1992', 'Understanding PostgresSQL', 'Study', 'SQL', 1),
-('Rose Leslie','Twitter without the Ego', 'Build', 'React', 3),
-('McLovin-0147','Dating app', 'Pair', 'JavaScript', 1),
-('Sk8_Boy_2000','What is JavaScript?', 'Study', 'JavaScript', 2),
-('AstonKutcher','Find my Car minigame app', 'Build', 'JavaScript', 1),
-('Craig','All I want for my birthday is my voice back', 'Build', 'React', 1),
+('Jimmy08','Lord of the Rings API', 'Build', 'React', 1);
+
+INSERT INTO posts
+(userName, projectGoal, projectType, projectTools, collaborators)
+VALUES
+('Lion_KingFan_1992', 'Understanding PostgresSQL', 'Study', 'SQL', 1);
+
+INSERT INTO posts
+(userName, projectGoal, projectType, projectTools, collaborators)
+VALUES
+('Rose Leslie','Twitter without the Ego', 'Build', 'React', 3);
+
+INSERT INTO posts
+(userName, projectGoal, projectType, projectTools, collaborators)
+VALUES
+('McLovin-0147','Dating app', 'Pair', 'JavaScript', 1);
+
+INSERT INTO posts
+(userName, projectGoal, projectType, projectTools, collaborators)
+VALUES
+('Sk8_Boy_2000','What is JavaScript?', 'Study', 'JavaScript', 2);
+
+INSERT INTO posts
+(userName, projectGoal, projectType, projectTools, collaborators)
+VALUES
+('AstonKutcher','Find my Car minigame app', 'Build', 'JavaScript', 1);
+
+INSERT INTO posts
+(userName, projectGoal, projectType, projectTools, collaborators)
+VALUES
+('Craig','All I want for my birthday is my voice back', 'Build', 'React', 1);
+
+INSERT INTO posts
+(userName, projectGoal, projectType, projectTools, collaborators)
+VALUES
 ('Steph','Make awesome UI', 'Pair', 'CSS', 2);
