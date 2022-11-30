21bd2add3964e57faff8979381718e961b7a2f5f
Colin Fraser <colin.fraser+schoolofcode@gmail.com>
2022-11-30 18:07:33 +0000

    Altered the github actions workflow to use prettier

--- db/index.js
+++ db/index.js
@@ -1,11 +1,11 @@
-import pg from "pg"
+import pg from "pg";
 
-const { Pool } = pg 
+const { Pool } = pg;
 
 const pool = new Pool({
-    connectionString:process.env.POSTGRES_CONNECTION_URL
-})
+  connectionString: process.env.POSTGRES_CONNECTION_URL,
+});
 
-export default function query(text,params){
-    return pool.query(text,params)
+export default function query(text, params) {
+  return pool.query(text, params);
 }
